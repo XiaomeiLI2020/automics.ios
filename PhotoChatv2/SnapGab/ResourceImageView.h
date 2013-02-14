68cc44cccd1847c73fef3da57dbc4109e73cf552
Shakir Ali <shakirmithani@gmail.com>
2013-02-14 16:47:46 +0000

    Added resource scale, translate functionality.

---
+++ PhotoChatv2/SnapGab/ResourceImageView.h
@@ -0,0 +1,13 @@
+//
+//  resourceImage.h
+//  scaleView
+//
+//  Created by horizon on 11/02/2013.
+//  Copyright (c) 2013 horizon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResourceImageView : UIView
+- (id)initWithFrame:(CGRect)frame image:(UIImage*)image;
+@end
