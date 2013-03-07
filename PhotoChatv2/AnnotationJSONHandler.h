8dd654e5392dc08577f06419a3033df4f9f63d9b
urashidmir <umer.rashid.mir@gmail.com>
2013-03-07 13:13:27 +0000

    Panel Post API included

---
+++ PhotoChatv2/AnnotationJSONHandler.h
@@ -0,0 +1,17 @@
+//
+//  AnnotationHandler.h
+//  scaleView
+//
+//  Created by horizon on 21/02/2013.
+//  Copyright (c) 2013 horizon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Annotation.h"
+
+@interface AnnotationJSONHandler : NSObject
+
++(Annotation*)getAnnotationFromAnnotationJSON:(NSDictionary*)annotationJSON;
++(NSArray*)getAnnotationsFromAnnotationsJSON:(NSArray*)annotationsJSON;
+
+@end
